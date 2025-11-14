45d29d5674de34fbed0dd8b97276784ba324bdcf
Akshay <149981521+akbhard@users.noreply.github.com>
2025-11-14 17:35:13 -0500

    Cleaned up code and handled preventing changes after winner has been announced

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
-const changePlayer = (() => {
+const gameBoard = (() => {
     let player = ""
+    let positions = ["", "", "", "", "", "", "", "", ""]
+    let isWinnerDeclared = false;
+    const board = document.getElementById("game-board")
 
-    const changeSymbol = () => {
+    const updateSquare = () => {
         if (player === "" || player === "O") {
             player = "X"
+            return player
         } else if (player === "X") {
             player = "O"
+            return player
         }
     }
 
-    const updateSquare = () => {
-        changeSymbol();
-        return player
+    const checkWinner = () => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        const winningMessage = document.getElementById("winning-message")
+
+        for (let i=0; i < winConditions.length; i++) {
+            let currentWinCondition = winConditions[i]
+            if (gameBoard.positions[currentWinCondition[0]] === "") {
+                continue;
+            } 
+            
+            if (
+                gameBoard.positions[currentWinCondition[0]]=== gameBoard.positions[currentWinCondition[1]] &&
+                gameBoard.positions[currentWinCondition[1]] === gameBoard.positions[currentWinCondition[2]]){
+                    winningMessage.textContent = `${gameBoard.positions[currentWinCondition[0]]} is the winner!`
+                    isWinnerDeclared = true;
+                    // console.log(`${gameBoard.positions[currentWinCondition[0]]} is the winner!`)
+            break;
+            }
+        }
     }
-    
-    return {updateSquare}
-})();
 
-
-const gameBoard = (() => {
-    let positions = ["", "", "", "", "", "", "", "", ""]
-    const board = document.getElementById("game-board")
-    
     function createGameBoard(){
         positions.forEach((tile, index) => {
         let square = document.createElement("div")
+
         square.setAttribute('id',`${index}`)
         square.setAttribute('class', 'square')
+        
         square.addEventListener("click", function (){
-            currentPlayer = changePlayer.updateSquare();
-            if (square.textContent != ""){
+            currentPlayer = updateSquare();
+            if (square.textContent != "" && isWinnerDeclared == false){
                 // alert("That square has been populated you idiot")
-                changePlayer.updateSquare();
-            } else {
+                updateSquare();
+            } else if (isWinnerDeclared == false) {
                 square.textContent = currentPlayer;
                 positions[index] = currentPlayer;
-                console.log(positions)
+                checkWinner();
+            } else {
+                alert("Winner has been declared. Game over! Please reset the game!")
             }
         })
 
@@ -46,3 +73,4 @@ const gameBoard = (() => {
 })();
 
 gameBoard.createGameBoard();
+
